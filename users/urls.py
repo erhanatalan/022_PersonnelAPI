872f203678b918cfda538a4fce1f0dcdb053380e
erhanatalan <erhanatalan2013@gmail.com>
2023-05-30 22:55:51 +0300

    auto profile

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from .views import RegisterView, ProfileUpdateView, logout
 
 
 urlpatterns = [
+    path('auth/logout/', logout),
     path('auth/', include('dj_rest_auth.urls')),
     path('register/', RegisterView.as_view()),
     path('profile/<int:pk>/', ProfileUpdateView.as_view()),
-    path('auth/logout/', logout)
 ]
